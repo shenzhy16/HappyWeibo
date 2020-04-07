1e736474d7d7b827b5a40c632820d31c9d3a6780
shenzhy16 <shenzhy16@gmail.com>
2020-04-08 00:06:56 +0800

    新增支持share

--- transfer.sh
+++ transfer.sh
@@ -1,2 +1,11 @@
-sed "s/|/\"},{\"inHome\":true,\"inHot\":true,\"inStatusComment\":true,\"word\":\"/g" list.txt>ShieldWords.bak #替换|
+echo -n \| > temp.txt 
+cat list_KW.txt temp.txt list_ID.txt > ShieldWords.bak #两个列表间用|连接
+rm temp.txt
+sed -i "s/|/\"},{\"inHome\":true,\"inHot\":true,\"inStatusComment\":true,\"word\":\"/g" ShieldWords.bak #替换|
 sed -i '/./{s/^/[\{\"inHome\":true,\"inHot\":true,\"inStatusComment\":true,\"word\":\"&/;s/$/&\"\}]/}' ShieldWords.bak #补齐前后
+sed '/./{s/^/\{\"b\":\"&/;s/$/&\",\"g\":0\}\n/}' list_KW.txt > temp1.txt
+sed -i "s/|/\",\"g\":0}\n{\"b\":\"/g" temp1.txt 
+sed '/./{s/^/\{\"b\":\"&/;s/$/&\",\"g\":1\}\n/}' list_ID.txt > temp2.txt
+sed -i "s/|/\",\"g\":1}\n{\"b\":\"/g" temp2.txt 
+cat temp1.txt temp2.txt > ShieldWords.share_backup
+rm temp1.txt temp2.txt
