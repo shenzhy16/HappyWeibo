98713aa9392df2ceac2bd58ee1b173cf9309ff90
shenzhy16 <shenzhy16@gmail.com>
2020-04-08 17:31:24 +0800

    重写了脚本

--- transfer.sh
+++ transfer.sh
@@ -1,11 +1,8 @@
-echo -n \| > temp.txt 
-cat list_KW.txt temp.txt list_ID.txt > ShieldWords.bak #两个列表间用|连接
-rm temp.txt
-sed -i "s/|/\"},{\"inHome\":true,\"inHot\":true,\"inStatusComment\":true,\"word\":\"/g" ShieldWords.bak #替换|
+echo > temp.txt #创建一个只有空行的文件
+cat list_KW.txt temp.txt list_ID.txt > ShieldWords.bak #连接
+sed -i ":a;N;s/\n/\"},{\"inHome\":true,\"inHot\":true,\"inStatusComment\":true,\"word\":\"/g;ta" ShieldWords.bak #替换换行符
 sed -i '/./{s/^/[\{\"inHome\":true,\"inHot\":true,\"inStatusComment\":true,\"word\":\"&/;s/$/&\"\}]/}' ShieldWords.bak #补齐前后
-sed '/./{s/^/\{\"b\":\"&/;s/$/&\",\"g\":0\}\n/}' list_KW.txt > temp1.txt
-sed -i "s/|/\",\"g\":0}\n{\"b\":\"/g" temp1.txt 
-sed '/./{s/^/\{\"b\":\"&/;s/$/&\",\"g\":1\}\n/}' list_ID.txt > temp2.txt
-sed -i "s/|/\",\"g\":1}\n{\"b\":\"/g" temp2.txt 
-cat temp1.txt temp2.txt > ShieldWords.share_backup
-rm temp1.txt temp2.txt
+sed '/./{s/^/\{\"b\":\"&/;s/$/&\",\"g\":0\}/}' list_KW.txt > temp1.txt
+sed '/./{s/^/\{\"b\":\"&/;s/$/&\",\"g\":1\}/}' list_ID.txt > temp2.txt
+cat temp1.txt temp.txt temp2.txt temp.txt > ShieldWords.share_backup
+rm temp.txt temp1.txt temp2.txt
