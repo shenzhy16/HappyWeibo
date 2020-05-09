ef9aebf89d14d1714a08953239ca9161151a582a
shenzhy16 <shenzhy16@gmail.com>
2020-05-09 09:58:40 +0800

    新增日志
    每次运行之后产生日志，可以看到之前的数量；但是还不会写怎么从日志里提取

--- transfer.sh
+++ transfer.sh
@@ -2,9 +2,8 @@
 sed  -i '/^$/d' list_KW.txt list_ID.txt #删掉文件中的空行,防止出现屏蔽词为空白的情况
 sort  -u list_KW.txt -o list_KW.txt; sort  -u list_ID.txt  -o list_ID.txt #对关键词文件进行排序，并删去重复词
 Num_ID=$(wc -l < list_ID.txt) ; Num_KW=$(wc -l < list_KW.txt)
-echo "已重新排序并去除重复词，共计："
-echo "${Num_KW}个屏蔽关键词"
-echo -e "${Num_ID}个屏蔽账户名\n"
+date |tee -a log
+echo -e "\n已重新排序并去除重复词，共计：\n${Num_KW}个屏蔽关键词\n${Num_ID}个屏蔽账户名"|tee -a log
 cat list_KW.txt list_ID.txt > ShieldWords.bak #连接
 sort  -u ShieldWords.bak -o ShieldWords.bak #排序并删去关键词与账户名间的重复词
 sed -i ":a;N;s/\n/\"},{\"inHome\":true,\"inHot\":true,\"inStatusComment\":true,\"word\":\"/g;ta" ShieldWords.bak #替换换行符
@@ -14,5 +13,5 @@ sed '/./{s/^/\{\"b\":\"&/;s/$/&\",\"g\":0\}/}' list_KW.txt > temp1.txt
 sed '/./{s/^/\{\"b\":\"&/;s/$/&\",\"g\":1\}/}' list_ID.txt > temp2.txt
 cat temp1.txt temp2.txt > ShieldWords.share_backup
 Size_See=$(du -h ShieldWords.bak);Size_Share=$(du -h ShieldWords.share_backup)
-echo -e "屏蔽词文件已生成：\nName\tSize\tFile\nSee\t${Size_See}\nShare\t${Size_Share}"
+echo -e "\n屏蔽词文件已生成：\nName\tSize\tFile\nSee\t${Size_See}\nShare\t${Size_Share}\n-----------------------------------------"|tee -a log
 rm temp1.txt temp2.txt
